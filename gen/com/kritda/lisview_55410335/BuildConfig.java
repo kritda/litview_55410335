f1a772ddeb07a4124c4367fe17270ac66712dff7
kritda <kritda.27@gmail.com>
2014-09-04 15:52:28 +0700

    bank

---
+++ gen/com/kritda/lisview_55410335/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kritda.lisview_55410335;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/kritda/lisview_55410335/List_items.java
@@ -0,0 +1,21 @@
+package com.kritda.lisview_55410335;
+
+import android.graphics.drawable.Drawable;
+
+public class List_items {
+	private Drawable drwable;
+	private String title;
+	public Drawable getDrwable() {
+		return drwable;
+	}
+	public void setDrwable(Drawable drwable) {
+		this.drwable = drwable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
